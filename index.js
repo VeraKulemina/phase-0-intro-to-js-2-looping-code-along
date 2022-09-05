d49675e45530b63fdf54350d5b02f46f62374ebc
Vera Kulemina <verakulemina@gmail.com>
2022-09-05 11:34:02 -0600

    Assignment push to repo

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const cards = [];
+cards.unshift("Charlie", "Samip", "Ali");
+function writeCards(cards, event) {
+    const message = [];
+    for (let a = 0; a < cards.length; a++){
+        message.push(`Thank you, ${cards[a]} for the wonderful ${event} gift!`);
+       
+    }
+    return message;
+} 
+console.log(writeCards(cards, "birthday"));
+
+let number = 5;
+debugger;
+function countDown(number) {
+    while (number > 0) {
+    console.log(number--);
+    debugger;
+
+}
+    return number;
+}
+countDown(number)
